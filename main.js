b17f46465e7bfef74c4791edcb7b58967bb0b898
Jesper van den Ende <jespertheend@gmail.com>
2021-11-14 22:48:39 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-invalid-this */
+import MagicString from "magic-string";
+
+/** @type {import("rollup").PluginImpl} */
+export default function resolveUrlObjects() {
+	return {
+		name: "resolve-url-objects",
+		async transform(code, id) {
+			if (!code.includes("@rollup-plugin-resolve-url-objects")) return null;
+
+			const magicString = new MagicString(code);
+			const re = /@rollup-plugin-resolve-url-objects(?<options>.*)[\s\S]+?new\s+URL\s*\((?<originalUrl>'.*'|".*")/gid;
+			for (const match of code.matchAll(re)) {
+				const startUrlIndex = match.indices.groups.originalUrl[0];
+				const endUrlIndex = match.indices.groups.originalUrl[1];
+				const originalUrl = match.groups.originalUrl.slice(1, -1);
+
+				const workerResolveResult = await this.resolve(originalUrl, id);
+				const chunkRefId = this.emitFile({
+					type: "chunk",
+					id: workerResolveResult.id,
+				});
+				const newUrl = `import.meta.ROLLUP_FILE_URL_${chunkRefId}`;
+				magicString.overwrite(startUrlIndex, endUrlIndex, newUrl);
+			}
+
+			return {
+				code: magicString.toString(),
+				map: magicString.generateMap({hires: true}),
+			};
+		},
+	};
+}
